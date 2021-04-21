71cad1083ebb7539baf3b8414d5bbeebde9403b2
gkrialashvili <giorgi.krialashvili08b@geolab.edu.ge>
2021-04-21 23:56:08 +0400

    main page half done

--- assets/scripts.js
+++ assets/scripts.js
@@ -231,84 +231,8 @@ Version      : 1.6
 	
 		);
 	}
-	
-  	/*===================================*
-	05. ROAD MAP SLIDER JS
-	*===================================*/
-	 $('.roadmap').owlCarousel({
-	     loop: false,
-	     margin: 30,
-		 autoHeight: true,
-	     nav: true,
-	     navText: ['<i class="ion-ios-arrow-back"></i>', '<i class="ion-ios-arrow-forward"></i>'],
-	     responsive: {
-	         0: {
-	             items: 1,
-	         },
-	         380: {
-	             items: 2,
-	         },
-	         600: {
-	             items: 3,
-				 margin: 15
-	         },
-	         1000: {
-	             items: 5,
-	         },
-	         1199: {
-	             items: 6
-	         }
-	     }
-	 });
-	 
-	 $('.roadmap_wrap').owlCarousel({
-	     loop: false,
-	     margin: 30,
-		 autoHeight: true,
-	     nav: true,
-	     navText: ['<i class="ion-arrow-left-c"></i>', '<i class="ion-arrow-right-c"></i>'],
-	     responsive: {
-	         0: {
-	             items: 1,
-	         },
-	         380: {
-	             items: 2,
-	         },
-	         767: {
-	             items: 3,
-	         },
-	         1000: {
-	             items: 4,
-	         },
-	         1199: {
-	             items: 5
-	         }
-	     }
-	 });
-	 $('.timeline').owlCarousel({
-	     loop: false,
-	     margin: 30,
-		 autoHeight: true,
-	     nav: true,
-	     navText: ['<i class="ion-ios-arrow-back"></i>', '<i class="ion-ios-arrow-forward"></i>'],
-	     responsive: {
-	         0: {
-	             items: 1,
-	         },
-	         380: {
-	             items: 2,
-	         },
-	         767: {
-	             items: 3,
-	         },
-	         1000: {
-	             items: 4,
-	         },
-	         1199: {
-	             items: 5
-	         }
-	     }
-	 });
+
+
 	 
 	/*===================================*
 	06. BLOG SLIDER S
@@ -430,50 +354,41 @@ Version      : 1.6
 		}, 600);
 		return false;
 	});
+
 	
-	/*===================================*
-	12. POPUP JS
-	*===================================*/
-	$('.content-popup').magnificPopup({
-		type: 'inline',
-		preloader: true,
-		mainClass: 'mfp-zoom'
-	});
+	// /*===================================*
+	// 13. ANIMATION JS
+	// *===================================*/
+	// $(function() {
+	// 	function ckScrollInit(items, trigger) {
+	// 		items.each(function() {
+	// 			var ckElement = $(this),
+	// 				AnimationClass = ckElement.attr('data-animation'),
+	// 				AnimationDelay = ckElement.attr('data-animation-delay');
 	
-	/*===================================*
-	13. ANIMATION JS
-	*===================================*/
-	$(function() {
+	// 			ckElement.css({
+	// 				'-webkit-animation-delay': AnimationDelay,
+	// 				'-moz-animation-delay': AnimationDelay,
+	// 				'animation-delay': AnimationDelay,
+	// 				opacity: 0
+	// 			});
 	
-		function ckScrollInit(items, trigger) {
-			items.each(function() {
-				var ckElement = $(this),
-					AnimationClass = ckElement.attr('data-animation'),
-					AnimationDelay = ckElement.attr('data-animation-delay');
+	// 			var ckTrigger = (trigger) ? trigger : ckElement;
 	
-				ckElement.css({
-					'-webkit-animation-delay': AnimationDelay,
-					'-moz-animation-delay': AnimationDelay,
-					'animation-delay': AnimationDelay,
-					opacity: 0
-				});
+	// 			ckTrigger.waypoint(function() {
+	// 				ckElement.addClass("animated").css("opacity", "1");
+	// 				ckElement.addClass('animated').addClass(AnimationClass);
+	// 			}, {
+	// 				triggerOnce: true,
+	// 				offset: '90%'
+	// 			});
+	// 		});
+	// 	}
 	
-				var ckTrigger = (trigger) ? trigger : ckElement;
+	// 	ckScrollInit($('.animation'));
+	// 	ckScrollInit($('.staggered-animation'), $('.staggered-animation-wrap'));
 	
-				ckTrigger.waypoint(function() {
-					ckElement.addClass("animated").css("opacity", "1");
-					ckElement.addClass('animated').addClass(AnimationClass);
-				}, {
-					triggerOnce: true,
-					offset: '90%'
-				});
-			});
-		}
-	
-		ckScrollInit($('.animation'));
-		ckScrollInit($('.staggered-animation'), $('.staggered-animation-wrap'));
-	
-	});
+	// });
 	
 	/*===================================*
 	14. START COUNTUP JS
@@ -506,38 +421,7 @@ Version      : 1.6
 			$('#' + $(this).val()).addClass('show active');
 		});
 	});
-	
-	google.maps.event.addDomListener(window, 'load', init);
-        
-            function init() {
-                // Basic options for a simple Google Map
-                // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-                var mapOptions = {
-                    // How zoomed in you want the map to start at (always required)
-                    zoom: 12,
 
-                    // The latitude and longitude to center the map (always required)
-                    center: new google.maps.LatLng(40.7020, -73.9220), // New York
-
-                    // How you would like to style the map. 
-                    // This is where you would paste any style found on Snazzy Maps.
-                   styles:[{"featureType":"all","elementType":"geometry.fill","stylers":[{"weight":"2.00"}]},{"featureType":"all","elementType":"geometry.stroke","stylers":[{"color":"#9c9c9c"}]},{"featureType":"all","elementType":"labels.text","stylers":[{"visibility":"on"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"landscape","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"color":"#eeeeee"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#7b7b7b"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#46bcec"},{"visibility":"on"}]},{"featureType":"water","elementType":"geometry.fill","stylers":[{"color":"#c8d7d4"}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"color":"#070707"}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]}]};
-
-                // Get the HTML DOM element that will contain your map 
-                // We are using a div with id="map" seen below in the <body>
-                var mapElement = document.getElementById('map');
-
-                // Create the Google Map using our element and options defined above
-                var map = new google.maps.Map(mapElement, mapOptions);
-
-                // Let's also add a marker while we're at it
-                var marker = new google.maps.Marker({
-                    position: new google.maps.LatLng(40.7020, -73.9220),
-                    map: map,
-					icon: "assets/images/marker.png",
-                    title: 'Snazzy!'
-                });
-            }	
 			
 	/*Demo js*/
 	$( window ).on( "load", function() {
